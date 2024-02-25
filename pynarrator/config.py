681529ec15e36289f764d678faa06746b526b129
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-02-25 20:27:40 -0300

    add screen options

--- pynarrator/config.py
+++ pynarrator/config.py
@@ -4,3 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     dialog_path:str = "/home/neumann/Desktop/oppaiman/dialog"
+    image_path:str = "/home/neumann/Desktop/oppaiman/img"
+
+    weight : int = 640
+    height : int = 480
