3291b69ca792d674c9bd8eb907babf198ce583b9
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-03-03 19:34:44 -0300

    add support to pyinstaller paths

--- pynarrator/config.py
+++ pynarrator/config.py
@@ -1,16 +1,35 @@
+import os
+import sys
+
 from dataclasses import dataclass
+from pathlib import Path
 
 
 @dataclass
 class Config:
-    source_dialog_path: str = "/home/neumann/Desktop/oppaiman/dialog"
-    dialog_path: str = "/home/neumann/Desktop/oppaiman/DIALOG"
-
-    image_path: str = "/home/neumann/Desktop/oppaiman/img"
-    save_path: str = "/home/neumann/Desktop/oppaiman/save"
+    source_dialog_path: str = "./dialog"
+    dialog_path: str = "./translated_dialog"
+    image_path: str = "./img"
+    save_path: str = "./save"
 
     width: int = 640
     height: int = 480
 
-    languages = ("english", "portuguese", "german")
+    languages = ["english", "portuguese", "german"]
     language = "portuguese"
+
+    def __post_init__(self) -> None:
+        "check if pyinstaller tmp data directory is present and set the game data paths"
+        try:
+            base_path = sys._MEIPASS
+        except AttributeError:
+            return
+
+        self.source_dialog_path = os.path.join(base_path, "dialog")
+        self.dialog_path = os.path.join(base_path, "translated_dialog")
+        self.image_path = os.path.join(base_path, "img")
+        self.save_path = os.path.join(base_path, "save")
+        self.languages = self.get_languages()
+
+    def get_languages(self):
+        return [p.name for p in Path(self.dialog_path).glob("*") if p.is_dir()]
