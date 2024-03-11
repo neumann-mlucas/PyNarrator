a22afb7086f50febfc6675414ffdd80dcd95e42b
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-03-11 18:16:44 -0300

    small refactor in main game loop to be more dry

--- pynarrator/main.py
+++ pynarrator/main.py
@@ -5,6 +5,13 @@ from controller import (GameController, GameState, LanguageMenuController,
 from model import DialogFacade
 from view import GameView, LanguageMenuView, MenuView, NameScreenView
 
+CLASS_STATE_MAP = (
+    (GameState.Menu, MenuView, MenuController),
+    (GameState.LanguageMenu, LanguageMenuView, LanguageMenuController),
+    (GameState.NameScreen, NameScreenView, NameScreenController),
+    (GameState.Game, GameView, GameController),
+)
+
 
 def init_screen(config: Config):
     pygame.init()
@@ -25,17 +32,11 @@ def main():
     model = DialogFacade(config)
 
     # Initialize components
-    menu_view = MenuView(config, model, screen)
-    menu_controller = MenuController(config, model, menu_view)
-
-    language_view = LanguageMenuView(config, model, screen)
-    language_controller = LanguageMenuController(config, model, language_view)
-
-    name_screen_view = NameScreenView(config, model, screen)
-    name_screen_controller = NameScreenController(config, model, name_screen_view)
-
-    game_view = GameView(config, model, screen)
-    game_controller = GameController(config, model, game_view)
+    controllers = {}
+    for state, view_class, controler_class in CLASS_STATE_MAP:
+        view = view_class(config, model, screen)
+        controller = controler_class(config, model, view)
+        controllers[state] = controller
 
     # Main loop
     running = True
@@ -46,23 +47,17 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 break
-            elif current_state == GameState.Menu:
-                controller = menu_controller
-            elif current_state == GameState.Game:
-                controller = game_controller
-            elif current_state == GameState.LanguageMenu:
-                controller = language_controller
-            elif current_state == GameState.NameScreen:
-                controller = name_screen_controller
 
+            # get appropriate controller
+            current_controller = controllers[current_state]
             # sync controler state and current_state variable
-            controller.state = current_state
+            current_controller.state = current_state
             # send event to the controller
-            controller.handle_events(event)
+            current_controller.handle_events(event)
             # update current_state variable state
-            current_state = controller.state
+            current_state = current_controller.state
             # render screen
-            controller.view.render()
+            current_controller.view.render()
 
         pygame.display.flip()
 
